599f760e484898e15a2c42e34056e87bf1772d7b
ratatosk87 <155539524+ratatosk87@users.noreply.github.com>
2024-01-03 14:22:55 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,22 @@
+﻿import sqlite3
+
+class ProductDB:
+    def __init__(self, connect: sqlite3.Connection) -> None:
+        self.__connect = connect
+        self.__cursor = connect.cursor()
+
+
+    def get_all_products(self):
+        sql = "SELECT name, price, description, img, id FROM coffee_products"
+        try:
+            self.__cursor.execute(sql)
+            return self.__cursor.fetchall()
+        except:
+            print("ошибка чтения из базы данных")
+            return []
+
+    def get_product(self, id):
+        sql = "SELECT * FROM coffee_products WHERE id = ?"
+        self.__cursor.execute(sql, tuple([id]))
+        return self.__cursor.fetchone()
+    
---
+++ server.py
@@ -0,0 +1,67 @@
+﻿from flask import Flask, render_template, g
+import sqlite3
+import database
+
+app = Flask(__name__)
+
+app.config['DATABASE'] = "static/bd/coffee.db"
+app.secret_key = "abc748596"
+
+nav_menu = [
+    {"link": "/index", "name": "Главная"},   
+    {"link": "/info", "name": "О нас"},
+    {"link": "/blog", "name": "Блог"},
+    {"link": "/shop", "name": "Магазин"},
+    {"link": "/contacts", "name": "Контакты"}
+]
+
+def connect_db():
+    connect = sqlite3.connect(app.config['DATABASE'])
+    connect.row_factory = sqlite3.Row
+    return connect
+
+def get_connect():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template("index.html", menu=nav_menu)
+
+@app.route("/info")
+def info():
+    return render_template("info.html", menu=nav_menu)
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html", menu=nav_menu)
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", menu=nav_menu)
+
+@app.route("/shop")
+def shop():
+    connect = get_connect()
+    base = database.ProductDB(connect)
+    return render_template("shop.html", menu=nav_menu, cards=base.get_all_products())
+
+
+@app.route("/shop/<int:value>")
+def prod(value):
+    connect = get_connect()
+    base = database.ProductDB(connect)
+    product = base.get_product(value)
+    return render_template("product.html", img=product["img"], price=product["price"], 
+                           desc=product["description"], name=product["name"],  menu=nav_menu)
+
+
+@app.teardown_appcontext
+def close_connect(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+if __name__ == "__main__":
+    app.run()
